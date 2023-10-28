43b75a9020984d54f0e421848f15a68c01b6b2bf
Tapka3000 <circle.table@yandex.ru>
2023-10-28 20:03:59 +0300

    Commit

--- src/main/java/org/example/Ipoteka.java
+++ src/main/java/org/example/Ipoteka.java
@@ -5,6 +5,8 @@ public class Ipoteka {
         int cost = 2500000;
         double firstPayment = 20.0;
         double firstContibution = (double)(cost / 100) * firstPayment;
+        System.out.println("Сумма ипотеки " + cost);
+        System.out.println("Первоначальный платёж " + firstPayment + " %");
         System.out.println("Первоначальный взнос " + firstContibution + " рублей");
     }
 }
