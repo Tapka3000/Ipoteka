6f259c5c6262ef8a6b4a637cbf888ebcb1a59e91
Tapka3000 <circle.table@yandex.ru>
2023-11-02 11:08:38 +0300

    Commit

--- src/main/java/Mortgage.java
+++ src/main/java/Mortgage.java
@@ -1,10 +1,9 @@
-package org.example;
-
-public class Ipoteka {
+public class Mortgage {
     public static void main(String[] args) {
         int cost = 2500000;
         double firstPayment = 20.0;
-        double firstContibution = (double)(cost / 100) * firstPayment;
+        double firstContibution = cost / 100 * firstPayment;
+
         System.out.println("Сумма ипотеки " + cost);
         System.out.println("Первоначальный платёж " + firstPayment + " %");
         System.out.println("Первоначальный взнос " + firstContibution + " рублей");
